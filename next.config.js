eca4af27563f034c6c000105d264920e985ff71d
yoshioka <yosshioka0022@gmail.com>
2024-07-27 16:57:48 +0900

    /blogsページにmdxファイルを出力する

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
-module.exports = {
+const withMDX = require('@next/mdx')()
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
   output: 'export',
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   images: {
     // MEMO: https://nextjs.org/docs/messages/export-image-api
     unoptimized: true
   }
 }
+
+module.exports = withMDX(nextConfig)
